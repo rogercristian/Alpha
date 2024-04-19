3e5cbc0a1f5d2c57304f96a3d97602899bf95e6c
Roger Cristian <erogennin@gmail.com>
2024-04-19 12:37:50 -0300

    Implementado logica de room

--- Assets/GAME/Script/Managers/GameManager.cs
+++ Assets/GAME/Script/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,33 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
 
-    private void Awake() => instance = this;
- 
+    [SerializeField] List<Room> rooms;
+    [SerializeField] CinemachineTargetGroup targetGroup;
 
+   [field:SerializeField] private Room currentRoom;
+
+    public Room CurrentRoom {
+        get { return currentRoom; } 
+        set
+        {
+            currentRoom?.DisableCamera();
+            currentRoom = value;
+            currentRoom.EnableCamera(targetGroup.transform);
+        }
+    }
+    private void Awake() => instance = this;
+
+    private void Start()
+    {
+        rooms.ForEach(room => room.OnRoomChange += HandlerRoomChange);
+    }
+    private void OnDestroy()
+    {
+        rooms.ForEach(room => room.OnRoomChange -= HandlerRoomChange);
+
+    }
+    private void HandlerRoomChange(Room room)
+    {
+        CurrentRoom = room;
+    }
 }
